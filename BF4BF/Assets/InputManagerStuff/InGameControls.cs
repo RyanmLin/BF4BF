35794819dfe48f87325b890158dd4e48ab694fcd
RyanmLin <64663423+RyanmLin@users.noreply.github.com>
2022-07-01 15:47:23 -0700

    Tadpole Controls Done

--- BF4BF/Assets/InputManagerStuff/InGameControls.cs
+++ BF4BF/Assets/InputManagerStuff/InGameControls.cs
@@ -9,6 +9,7 @@ public class InGameControls : MonoBehaviour
 
     // Script References
     private EggMovement eggMovement;
+    private TadpoleMovement tadpoleMovement;
 
     // Input State Variables
     private float verticalAxis;
@@ -16,6 +17,7 @@ public class InGameControls : MonoBehaviour
     void Awake()
     {
         eggMovement = GetComponent<EggMovement>();
+        tadpoleMovement = GetComponent<TadpoleMovement>();
 
         verticalAxis = 0f;
         inputActions = new InputManager();
@@ -27,6 +29,8 @@ public class InGameControls : MonoBehaviour
         
         inputActions.Player.HorizontalMovement.performed    += OnMoveInput;
         inputActions.Player.HorizontalMovement.canceled     += OnMoveCancel;
+        inputActions.Player.VerticalMovement.performed      += OnRiseInput;
+        inputActions.Player.VerticalMovement.canceled       += OnRiseCancel;
         inputActions.Player.Jump.performed                  += OnJumpInput;
     }
 
@@ -34,6 +38,8 @@ public class InGameControls : MonoBehaviour
     {
         inputActions.Player.HorizontalMovement.performed    -= OnMoveInput;
         inputActions.Player.HorizontalMovement.canceled     -= OnMoveCancel;
+        inputActions.Player.VerticalMovement.performed      -= OnRiseInput;
+        inputActions.Player.VerticalMovement.canceled       -= OnRiseCancel;
         inputActions.Player.Jump.performed                  -= OnJumpInput;
 
         inputActions.Disable();
@@ -59,6 +65,11 @@ public class InGameControls : MonoBehaviour
         {
             eggMovement.Move(value);
         }
+
+        if (tadpoleMovement != null) 
+        {
+            tadpoleMovement.Move(value);
+        }
     }
 
     private void OnMoveCancel(InputAction.CallbackContext context)
@@ -68,6 +79,43 @@ public class InGameControls : MonoBehaviour
         if(eggMovement != null)
         {
             eggMovement.Move(0f);
+        }
+
+        if (tadpoleMovement != null) 
+        {
+            tadpoleMovement.Move(0f);
+        }
+    }
+
+    private void OnRiseInput(InputAction.CallbackContext context)
+    {
+        var value = context.ReadValue<float>();
+        // Handling for gamepads to make equivalent to keyboard
+        // Keyboard is either on or off. Gamepad has range from 0 - 1
+        // TODO: See if we desire on/off or if we want to make keyboard function similar to joystick in some way
+        if (value >= 0.15f) {
+            value = 1f;
+        }
+        else if (value <= -0.15f) {
+            value = -1f;
+        }
+        else {
+            value = 0f;
+        }
+
+        if (tadpoleMovement != null) 
+        {
+            tadpoleMovement.Rise(value);
+        }
+    }
+
+    private void OnRiseCancel(InputAction.CallbackContext context)
+    {
+        var value = context.ReadValue<float>();
+
+        if(tadpoleMovement != null)
+        {
+            tadpoleMovement.Rise(0f);
         }
     }
 
